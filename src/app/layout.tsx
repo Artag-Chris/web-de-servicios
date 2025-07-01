76b4343a61b73488dca39b1818466d3ada078c92
Artag/Chris <scristxyz@gmail.com>
2025-06-30 19:30:47 -0500

    act

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export const metadata: Metadata = {
         width: 1200,
         height: 630,
         alt: "Artag desarrollo de software en Pereira",
-      }, 
+      },  
     ],  
   },
   // Twitter Card  
